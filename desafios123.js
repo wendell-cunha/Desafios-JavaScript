dee89d31c0c627ce88f9573af556883ef3685211
Wendell Henrique <wendell.claudino@gmail.com>
2022-01-24 14:06:23 -0300

    Desafios 1, 2 e 3

---
+++ desafios123.js
@@ -0,0 +1,105 @@
+console.log('Olá Mundo')
+console.log('primeiro')
+console.log('segundo')
+
+//Declarando variáveis
+
+//var - escopo global (quase não é usada)
+//let - escopo local (mais utilizada)
+//const - declaração de uma constante
+
+//DESAFIO 1
+
+let nome = 'Wendell'
+let numero = 1000
+let x = 2022 - numero
+
+console.log(x + numero)
+console.log('estou mexendo no console.log')
+quartFeira = false
+
+
+// // DESAFIO 2
+
+// //(1)
+// let idade = 20
+
+// if (idade >= 18) {
+//     console.log('Parabéns, você é maior de idade. Agora você é um jovem adulto !')
+// }
+// humano = true
+
+// //(2)
+// if (idade >= 18 && humano == true) {
+//     console.log('Ei, não é um ornitorrinco ! Você é um humano maior de 18 !')
+// }
+
+// //(3)
+
+// let aniversario = 'março'
+
+// if (aniversario == 'janeiro' || aniversario == 'Dezembro') {
+//     console.log('Você faz aniversário nas férias !')
+// }
+
+
+// //(4)
+// let a = 'Rendell'
+// console.log(a.length)
+
+// if (a.indexOf('R') == 0) {
+//     console.log('seu nome começa com R')
+// }else {
+//     console.log('seu nome nao começa com R')
+// }
+// // eu acho que tem algum comando pra pegar um determinado valor de uma string mas nao consegui. 
+// //Eu tentei fazer a.[0] == 'R' mas nao funcionou
+
+// //(5)
+
+// let n = 'Eendell'
+// if (a.length >= 6 || a.indexOf('E') == 0) {
+//     console.log('Seu nome tem mais de 6 letras ou começa com a letra E')
+// }
+
+// //DESAFIO 3
+
+// //(1)
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i)    
+// }
+
+// console.log('')
+// console.log('')
+// //(2)
+// let j = 10
+// while (j >= 1) {
+//     console.log(j)
+//     j--
+
+// }
+
+// console.log('')
+// console.log('')
+
+// //(3)
+
+// let a = 1
+// while (a <= 100) {
+//     if (a % 2 !== 0) {
+//         console.log(a)
+//     }
+//     a++
+// }
+
+// console.log('')
+// console.log('')
+// //(4)
+
+// let b = 0
+// while (b <= 100) {
+//     if (b % 2 == 0) {
+//         console.log(b)
+//     }
+//     b++
+// }
