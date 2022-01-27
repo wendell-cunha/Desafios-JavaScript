263c9fd633c33671c55d6258c45ffd26cc4f7624
Wendell Henrique <wendell.claudino@gmail.com>
2022-01-27 12:22:55 -0300

    Desafio 5 concluído

---
+++ desafio5.js
@@ -0,0 +1,32 @@
+//1 e 2
+let itens = ['preto', 'gato', 3.14, true, 'Wendell']
+itens.unshift('Ola Mundooo')
+console.log(itens)
+
+//3
+
+let itens1 = ['preto', 'gato', 3.14, true, 'Wendell']
+itens1.pop()
+console.log(itens1)
+
+//4
+
+let itens2 = ['preto', 'gato', 3.14, true, 'Wendell']
+itens2.push('Adicionando', 2)
+console.log(itens2)
+
+//5
+
+let itens3 = ['preto', 'gato', 3.14, true, 'Wendell']
+itens3.shift()
+console.log(itens3)
+
+// 6
+
+let numbers = [-Math.PI, Math.cos(0 * Math.PI / 180), -95, Math.LOG10E, 1 + Math.SQRT2 , 0]
+numbers.sort()
+console.log(numbers)
+
+//Eu pesquisei essas funções legais no W3 scholl 
+// Dá pra resolver essa parte sem usar esse método, usando if/else e for. Mas é bem mais dificil. 
+//Vou tentar resolver e posto quando conseguir
